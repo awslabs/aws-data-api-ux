f52ed7b8209854729608d5cba35ad1a722071140
Pavel Morshenyuk <pavelmk@amazon.com>
2021-11-26 14:51:01 +0000

    Libs updated

--- angular/src/app/app-services/resource.service.ts
+++ angular/src/app/app-services/resource.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 @Injectable()
